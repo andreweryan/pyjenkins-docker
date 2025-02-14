78665c8ecd9b96d75694d7d8b6ff95ec3354863d
Andrew Ryan <andrrvt15@gmail.com>
2025-02-13 23:46:19 -0500

    updating sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+sudo groupadd -f docker
+sudo usermod -aG docker $USER
+newgrp docker
+
 docker build -t jenkins-python .
-docker run -p 8080:8080 -p 50000:50000 --name jenkins-container jenkins-python
+docker run -p 8080:8080 -p 50000:50000 jenkins-python
